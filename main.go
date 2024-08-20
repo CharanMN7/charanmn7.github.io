03f27c980623f43eee69061c9637e1fb269ea5a9
Charan <charanxmn@gmail.com>
2024-08-21 03:16:02 +0530

    Tested the server to see if it's working.

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		http.ServeFile(w, r, "index.html")
+	})
+
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Could not start server: %s\n", err)
+	}
 }
