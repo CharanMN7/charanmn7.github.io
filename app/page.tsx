6a4f4ad7fcc3a88d40c1de62c286a64b228d95e9
Charan <charanxmn@gmail.com>
2025-02-28 04:04:49 +0530

    Add Desktop component to Home for enhanced layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Desktop from "@/components/os/desktop/desktop";
 import TopBar from "@/components/os/top-bar/top-bar";
 
 export default function Home() {
   return (
     <div className="h-screen w-screen">
       <TopBar />
+      <Desktop />
     </div>
   );
 }
