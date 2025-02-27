9a324949f9612a722d4ece85dfd750403880bafe
Charan <charanxmn@gmail.com>
2025-02-28 02:09:57 +0530

    Add TopBar component to Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import TopBar from "@/components/os/top-bar";
+
 export default function Home() {
   return (
-    <></>
+    <div className="h-screen w-screen">
+      <TopBar />
+    </div>
   );
 }
