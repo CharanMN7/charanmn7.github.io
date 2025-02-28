29d4c1bd39561b6a23fef8d7878cdcd1d0db0855
Charan <charanxmn@gmail.com>
2025-02-28 07:05:06 +0530

    Add MeCard component with profile picture and social media links

---
+++ components/cards/me.tsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import { Card, CardContent } from "../ui/card";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import xlogo from "@/public/xcom.svg";
+import instagramLogo from "@/public/instagram.svg";
+import youtubeLogo from "@/public/youtube.png";
+import githubLogo from "@/public/github.svg";
+
+export default function MeCard() {
+  return (
+    <Card className="w-fit">
+      <CardContent className="flex items-center space-x-4">
+        <Image
+          src="https://uvt8fr24f6.ufs.sh/f/vP5L84ZPMm9e4vDFCg9lXC92ZNpdreBKExyJizWuAkoU1OvS"
+          width={100}
+          height={100}
+          className="rounded-full"
+          alt="Charan's placeholder profile picture"
+        />
+
+        <div>
+          <h1 className="font-bold text-4xl">Charan</h1>
+
+          <Link href="mailto:hello@charan.dev" className="text-sm text-muted-foreground">
+            hello@charan.dev
+          </Link>
+
+          <div className="flex space-x-2 mt-2">
+            <Button variant="outline" size="icon" className="rounded-full" asChild>
+              <Link href="https://x.com/CharanMNX/">
+                <Image
+                  src={xlogo}
+                  alt="X"
+                  width={16}
+                  height={16}
+                />
+              </Link>
+            </Button>
+            <Button variant="outline" size="icon" className="rounded-full" asChild>
+              <Link href="https://iam.charan.dev/">
+                <Image
+                  src={instagramLogo}
+                  alt="Instagram"
+                  width={20}
+                  height={20}
+                />
+              </Link>
+            </Button>
+            <Button variant="outline" size="icon" className="rounded-full" asChild>
+              <Link href="https://www.youtube.com/@charan-dev">
+                <Image
+                  src={youtubeLogo}
+                  alt="YouTube"
+                  width={20}
+                  height={20}
+                />
+              </Link>
+            </Button>
+            <Button variant="outline" size="icon" className="rounded-full p-0" asChild>
+              <Link href="https://github.com/CharanXMNX">
+                <Image
+                  src={githubLogo}
+                  alt="GitHub"
+                  width={20}
+                  height={20}
+                />
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card >
+  );
+}
