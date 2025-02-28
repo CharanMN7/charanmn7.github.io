403c61cee01376d21b588d0cc6ae5de2142cbc18
Charan <charanxmn@gmail.com>
2025-02-28 07:45:26 +0530

    Add WorkExpCard component to Home page for displaying work experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import BioCard from "@/components/cards/bio";
 import MeCard from "@/components/cards/me";
+import WorkExpCard from "@/components/cards/work-exp";
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col items-center justify-center space-y-4">
       <MeCard />
       <BioCard />
+      <WorkExpCard />
     </div>
   );
 }
