3cec3dd48aa603f29c1f24fb07ccc87b6de5e6fa
Charan <charanxmn@gmail.com>
2025-02-28 07:25:26 +0530

    Add BioCard component to Home page for enhanced user information display

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import BioCard from "@/components/cards/bio";
 import MeCard from "@/components/cards/me";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="h-screen flex flex-col items-center justify-center space-y-4">
       <MeCard />
+      <BioCard />
     </div>
   );
 }
