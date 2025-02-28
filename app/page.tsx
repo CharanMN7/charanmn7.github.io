9057b6a61c4717aaaa743a989add23f6c0cce624
Charan <charanxmn@gmail.com>
2025-02-28 05:55:18 +0530

    Remove unused Desktop and TopBar components to streamline the application

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
-import Desktop from "@/components/os/desktop/desktop";
-import TopBar from "@/components/os/top-bar/top-bar";
-
 export default function Home() {
   return (
-    <div className="h-screen w-screen">
-      <TopBar />
-      <Desktop />
+    <div className="">
     </div>
   );
 }
--- components/os/desktop/desktop.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-export default function Desktop() {
-  return (
-    <div className="px-4 py-2 h-full w-full">
-      <h1>Desktop</h1>
-    </div>
-  )
-}
