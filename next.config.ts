1c77d996318620e2ed38c789854d400150e84ee1
Charan <charanxmn@gmail.com>
2025-04-20 14:35:44 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,18 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["uvt8fr24f6.ufs.sh", "cdn.buymeacoffee.com"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: "https://my-blog-three-virid.vercel.app/blog",
+      },
+      {
+        source: "/blog/:path*",
+        destination: "https://my-blog-three-virid.vercel.app/blog/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
